d2876986c502e4740c93c9af7a60f301eee9c735
JamesAbel <j@abel.co>
2024-03-13 11:21:33 -0700

    add Python bool/truthiness

--- o_no.py
+++ o_no.py
@@ -1,5 +1,6 @@
 from openpyxl import load_workbook
 from xlrd import open_workbook
+from tobool import to_bool
 
 wb = open_workbook('o_no.xls')
 ws = wb.sheet_by_index(0)
@@ -18,3 +19,16 @@ a_bool = bool(a)
 b_bool = bool(b)
 print(a, type(a), a_bool)
 print(b, type(b), b_bool)
+print()
+
+print("Not spreadsheet per se, but what Python does")
+print('bool(False)')
+print(bool(False))
+print('bool("False")')  # no exception!
+print(bool("False"))
+print('to_bool("False")')
+print(to_bool("False"))
+print()
+
+b_to_bool = to_bool(b)
+print(b_to_bool, type(b_to_bool))
